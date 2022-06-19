858acf3a7ff9215a962611c09757934bde644775
Lud <instimila@gmail.com>
2022-06-19 14:32:02 +0200

    Post tweets

--- app.js
+++ app.js
@@ -20,6 +20,17 @@ app.post("/sign-up",(req,res)=>{
     res.send("ok")
 })
 
+app.post("/tweets",(req,res)=>{
+  
+    tweet.username=req.body.username
+    tweet.tweet=req.body.tweet
+    tweet.avatar=usuario.avatar
+
+tweets.push(tweet)
+res.send("ok")
+})
+
+
 
 
 app.listen(5000,()=>{
